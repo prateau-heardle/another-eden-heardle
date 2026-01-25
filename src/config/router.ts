5bb0ddef490cf8b3f716a9a7a75529dcae0bbc6f
prateau <philippe.rateau@outlook.com>
2026-01-25 14:59:51 +0100

    feat: :rocket: add workflow for github page

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, defineRoute } from 'type-route'
 
+const BASE_URL = '/heardle-react'
+
 export enum ROUTES {
     DAILY = 'daily',
     INFINITE = 'infinite'
 }
 
 export const { RouteProvider, useRoute, routes } = createRouter({
-    [ROUTES.DAILY]: defineRoute('/'),
-    [ROUTES.INFINITE]: defineRoute('/infinite')
+    [ROUTES.DAILY]: defineRoute(BASE_URL),
+    [ROUTES.INFINITE]: defineRoute(BASE_URL + '/infinite')
 })
